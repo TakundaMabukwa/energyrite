a7773c50b59d76eeb90f42dcdfdd6bb35b76470b
TakundaMabukwa <mabukwatakunda@gmail.com>
2025-11-27 10:46:42 +0200

    UE

--- components/ui/fuel-gauge.tsx
+++ components/ui/fuel-gauge.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { cn } from '@/lib/utils';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
